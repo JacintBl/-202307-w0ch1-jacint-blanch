7b2cd010e136172728e8bb50c23050cdb3fc56d0
Jacint Blanch <jacintblanch5@gmail.com>
2023-07-07 10:21:47 +0200

    create function to generate deck

---
+++ index.js
@@ -0,0 +1,28 @@
+const deckCards = () => {
+  const cardSuit = ["♥️", "♠️", "♦️", "♣️"];
+  const cardValue = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  let deck = [];
+  cardValue.forEach((value) => {
+    cardSuit.forEach((suit) => {
+      let card = `${value} ${suit}`;
+      deck.push(card);
+    });
+  });
+
+  return deck;
+};
